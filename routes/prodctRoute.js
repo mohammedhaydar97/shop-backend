75025f75730d6ca961ca077feb8d404573596df6
mohammed haydar <mohammedhaydar97@gmail.com>
2024-01-24 23:44:56 +0200

    server initiation

--- routes/prodctRoute.js
+++ routes/prodctRoute.js
@@ -1,23 +1,10 @@
 import express from "express";
-import products from "./../products.js";
-import asyncHandler from "../middleware/asyncHandlre.js";
-import productModel from './../model/productModel.js'
-import Product from "./../model/productModel.js";
+import { getProductById, getProducts } from "./../controllers/productController.js";
 
 const router = express.Router();
 
-router.get('/',asyncHandler (async (req, res) => {
-    const products = await productModel.find({})
-    res.json(products);
-}));
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
-router.get('/:id',asyncHandler(async (req, res) => { 
-    const product = await Product.findById(req.params.id);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product not found' });
-    }
-}));
 
 export default router;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cors from 'cors'; 
 import connectDB from "./config/Db.js";
 import productsRoutes from './routes/prodctRoute.js'
+import {errorHandler,notFound} from './middleware/errorMiddleware.js'
+
 
 dotenv.config();
 connectDB();
@@ -13,6 +15,9 @@ app.use(cors());
 app.use('/api/products',productsRoutes)
 
 const port = process.env.PORT || 5000;
+
+app.use(errorHandler)
+app.use(notFound)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
